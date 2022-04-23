15025017ad7dd888720578d501fdb3c675999f80
Aaron Darcy <48316970+Aaron-Darcy@users.noreply.github.com>
2022-04-23 05:37:37 +0100

    Added Calorie Tracker

---
+++ components/NavBarComp.js
@@ -0,0 +1,61 @@
+//imports
+import React, { Component } from 'react'
+import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+//routing import
+import BMI from './BMI';
+import BMR from './BMR';
+import CalorieTracker from './CalorieComp/CalorieTracker';
+import Nutrition from './Nutrition';
+
+export default class NavBarComp extends Component {
+  render() {
+    return (
+      //router and navbar
+      <Router>
+        <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Gym Companion</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to={"/Nutrition"}>Nutrition</Nav.Link>
+                <Nav.Link as={Link} to={"/bmi"}>BMI Calculator</Nav.Link>
+                <Nav.Link as={Link} to={"/BMR"}>Calorie Calculator</Nav.Link>
+                <Nav.Link as={Link} to={"/CalorieTracker"}>Calorie Tracker</Nav.Link>
+                <NavDropdown title="Workouts" id="collasible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to={"/cardio"}>Cardio</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/ppl"}>Push/Pull/Legs</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/fullbody"}>Full Body</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link as={Link} to={"/LogIn"}>Log In</Nav.Link>
+                <Nav.Link eventKey={2} as={Link} to={"/SignUp"}>
+                  Sign Up
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+        
+        <div>
+        <Routes>
+          <Route path="/Nutrition" element={<Nutrition />}/>
+          <Route path="/BMI" element={<BMI />}/>
+          <Route path="/BMR" element={<BMR />}/>
+          <Route path="/CalorieTracker" element={<CalorieTracker />}/>
+        </Routes>
+        </div>
+      </Router>
+    )
+  }
+}
