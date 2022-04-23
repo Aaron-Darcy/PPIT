f3d6f49f1e58f729169965bd0fb0a446058e05e8
Aaron Darcy <48316970+Aaron-Darcy@users.noreply.github.com>
2022-04-23 06:14:16 +0100

    Calorie Tracker Added

---
+++ gymapp/src/App.js
@@ -0,0 +1,14 @@
+import './stylesheets/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComp from './components/NavBarComp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarComp></NavBarComp>
+    </div>
+  );
+}
+
+export default App;
